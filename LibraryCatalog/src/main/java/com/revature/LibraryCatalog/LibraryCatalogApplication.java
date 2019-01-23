6781a3c47d7d43ee53f2ab32d70627e3c8d35521
Carlos J Portillo <36525286+CarlosJPortillo@users.noreply.github.com>
2019-01-23 16:11:50 -0600

    Created Eclipse Spring folder

---
+++ LibraryCatalog/src/main/java/com/revature/LibraryCatalog/LibraryCatalogApplication.java
@@ -0,0 +1,14 @@
+package com.revature.LibraryCatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryCatalogApplication.class, args);
+	}
+
+}
+
---
+++ LibraryCatalog/src/main/java/com/revature/LibraryCatalog/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.revature.LibraryCatalog;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LibraryCatalogApplication.class);
+	}
+
+}
+
