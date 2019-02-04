21c271d0373e4beb73ab540c0aca85d03e31f669
Justice Fears <justice.fears@gmail.com>
2019-02-04 10:22:43 -0600

    trying to fix angular integrationg

--- LibraryCatalog/src/main/java/com/revature/LibraryCatalog/controller/LoginUserController.java
+++ LibraryCatalog/src/main/java/com/revature/LibraryCatalog/controller/LoginUserController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.google.gson.Gson;
 import com.revature.LibraryCatalog.dao.LibrarianDao;
@@ -130,5 +131,6 @@ public class LoginUserController {
 			return loginInfo;		
 		}
 	}
+	
 	
 }
